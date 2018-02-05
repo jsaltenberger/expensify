5a805b6c2c82a26b78c1a43af49074e24d0ba777
Joe <j.saltenberger@gmail.com>
2018-02-05 14:14:32 -0800

    Refactoring redux.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'rent', amount: 400, createdAt: 11000}));
+store.dispatch(addExpense({description: 'coffee', amount: 200, createdAt: -1000}));
+store.dispatch(setTextFilter('fee'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
