009009edd3264e0698c643d4d9177d1aae479383
Joe <j.saltenberger@gmail.com>
2018-02-16 12:06:05 -0800

    Setup webpack.config for production builds.

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Rent', amount: 800, createdAt: 200 }));
-store.dispatch(addExpense({ description: 'Coffee', amount: 100, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Water', amount: 300, createdAt: 500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
