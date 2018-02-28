67a1d9a40dcf2510f4e134da55cc21b230afce4a
Joe <j.saltenberger@gmail.com>
2018-02-28 15:18:23 -0800

    Add remove expense functionality.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,14 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, removeExpense, editExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {
+  startAddExpense,
+  addExpense,
+  startRemoveExpense,
+  removeExpense,
+  editExpense,
+  startSetExpenses,
+  setExpenses
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -22,7 +30,7 @@ test('should setup add expense action object with provided values', () => {
   });
 });
 
-test('should add expense to database and store', (done) => {
+test('should add expense to firebase and store', (done) => {
   const store = createMockStore({});
   const expenseData = {
     description: 'Mouse',
@@ -48,7 +56,7 @@ test('should add expense to database and store', (done) => {
   });
 });
 
-test('should add expense with defaults to database and store', (done) => {
+test('should add expense with defaults to firebase and store', (done) => {
   const store = createMockStore({});
   const expenseDefaults = {
     description: '',
@@ -79,6 +87,22 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
+  });
+});
+
+test('should remove expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
   });
 });
 
